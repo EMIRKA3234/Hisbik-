bd294cc6724091401e39594709204f7e02ed10a8
EMIRKA3234 <EMIRKA3234>
2024-05-29 18:16:19 +0300

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const sounds = ["appluase","boo", "gasp","tada","victory","wrong"];
+const buttons = document.getElementById("buttons");
+const stopSounds = () =>{
+    sounds.forEach((sound) => {
+const currentSound.pause();
+currentSound.currentTime = 0; 
+});
+}
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");   
+}
